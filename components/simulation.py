4f6dbba652dc7fcf2ffb5360ffdb36fc8103b588
Diego Souza <souza.claudiodiego@gmail.com>
2023-07-19 14:50:31 -0300

    exportando dados de mobilidade na forma [win, x, y, time]

--- components/simulation.py
+++ components/simulation.py
@@ -6,21 +6,21 @@ import traci
 
 from datetime import datetime
 
-from FLPUCI.pre_processing.datacleaning import DataCleaning
-
 
 class Simulation:
-    def __init__(self, scenario_path, simulation_time):
+    def __init__(self, scenario_path, simulation_time, window_size):
         """
         Instantiates and starts a simulation, exporting raw mobility data of each moving object to its respective file.
         It creates one directory for each simulation round, identified by yyyy-mm-dd-H-M-S
         """
         self.scenario_path = scenario_path
-        self.mobility_output = f'{self.scenario_path}/sim_{datetime.now().strftime("%y-%m-%d-%H-%M-%S")}'
-        if not os.path.exists(self.mobility_output):
-            os.makedirs(self.mobility_output)
+        self.trace_path = f'{self.scenario_path}/sim_{datetime.now().strftime("%y-%m-%d-%H-%M-%S")}'
+
+        if not os.path.exists(self.trace_path):
+            os.makedirs(self.trace_path)
 
         self.simulation_time = simulation_time
+        self.window_size = window_size
 
         self.sumoCmd = ['sumo', '-c', f'{self.scenario_path}/osm.sumocfg']
         self.run()
@@ -29,20 +29,24 @@ class Simulation:
         """
         runs a simulation
         """
-        cleaning = DataCleaning(self.mobility_output, 10)
         traci.start(self.sumoCmd)
+
+        current_window = 0
+
         while traci.simulation.getMinExpectedNumber() > 0 and traci.simulation.getTime() < self.simulation_time:
             traci.simulationStep()
 
             vehicles = traci.vehicle.getIDList()
 
-            self.write_trace(vehicles)
-            cleaning.run()
+            if traci.simulation.getTime() % self.window_size == 0:
+                current_window += 1
+
+            self.write_trace(vehicles, current_window)
 
         traci.close()
         time.sleep(5)
 
-    def write_trace(self, vehicles):
+    def write_trace(self, vehicles, current_window):
         """
         writes the current position of a moving object in its respective simulation file
         :param vehicles: the vehicles moving
@@ -51,9 +55,9 @@ class Simulation:
             vehid = vehicles[i]
             x, y = traci.vehicle.getPosition(vehicles[i])
 
-            record = [x, y, traci.simulation.getTime()]
+            record = [current_window, x, y, traci.simulation.getTime()]
 
-            vehicle_csv_file = os.path.join(self.mobility_output, f'{vehid}.csv')
+            vehicle_csv_file = os.path.join(self.trace_path, f'{vehid}.csv')
 
             with open(vehicle_csv_file, 'a', newline='') as file_csv:
                 writer = csv.writer(file_csv)
@@ -64,5 +68,7 @@ def arguments():
     parser = argparse.ArgumentParser(description='Required arguments to run SUMO simulations')
     parser.add_argument('--scenario_path', type=str, default='2023-07-13-15-23-35', help='The relative path of the '
                                                                                          'scenario')
-    parser.add_argument('--simulation_time', type=int, default='3600', help='The simulation time duration')
+    parser.add_argument('--simulation_time', type=int, default='7200', help='The simulation time duration')
+    parser.add_argument('--window_size', type=int, default='60', help='The time window size to generate mobility '
+                                                                     'samples')
     return parser.parse_args()
--- sumo_run_v1.py
+++ sumo_run_v1.py
@@ -3,5 +3,6 @@ from components.simulation import Simulation, arguments
 args = arguments()
 scenario_path = args.scenario_path
 simulation_time = args.simulation_time
+window_size = args.window_size
 
-Simulation(scenario_path, simulation_time)
+Simulation(scenario_path, simulation_time, window_size)
