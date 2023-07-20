1c2ed2d8cf621f51ffe936fb0464b159e427aefb
Diego Souza <souza.claudiodiego@gmail.com>
2023-07-20 15:25:32 -0300

    WIP: SampleHandler

--- FLPUCI/pre_processing/sample_generation.py
+++ FLPUCI/pre_processing/sample_generation.py
@@ -1,10 +1,22 @@
 import math
 import os.path
+import random
 
 import numpy as np
 import pandas as pd
 
 from FLPUCI.utils.helpers import sorted_files, get_file_path, dir_exists_create
+
+
+def logit(cells_list: list):
+    for j in range(len(cells_list)):
+        p = float(cells_list[j])
+        e = float(0)
+        if p == float(0) or p == float(1):
+            e += 0.000001
+        new_p = math.log((p + e) / ((1 - p) + e))
+        cells_list[j] = new_p
+    return cells_list
 
 
 class FeatureMatrix:
@@ -70,16 +82,66 @@ class FeatureMatrix:
         output_file.write(cell_stay_time)
 
     def normalize_by_window_size(self, cell_stay_time: np.array):
-        cells_stay_time_logit = self.logit([item / self.window_size for item in cell_stay_time])
+        cells_stay_time_logit = logit([item / self.window_size for item in cell_stay_time])
         cells_stay_time_str = ', '.join([str(item) for item in cells_stay_time_logit]) + '\n'
         return cells_stay_time_str
 
-    def logit(self, cells_list: list):
-        for j in range(len(cells_list)):
-            p = float(cells_list[j])
-            e = float(0)
-            if p == float(0) or p == float(1):
-                e += 0.000001
-            new_p = math.log((p + e) / ((1 - p) + e))
-            cells_list[j] = new_p
-        return cells_list
+
+def handle_pixels(sample_pixels: np.array):
+    if sample_pixels.max() != sample_pixels.min():
+        delta = sample_pixels.max() - sample_pixels.min()
+        pixels = np.absolute(sample_pixels.astype("float64")) / delta
+        pixels = 1 - pixels
+        pixels = pixels + np.absolute(pixels.min())
+    else:
+        pixels = np.absolute(sample_pixels) * 0
+    return pixels
+
+
+def reshape(pixels: np.array):
+    if len(pixels.shape) == 4:
+        pixels = np.reshape(pixels, (pixels.shape[0], pixels.shape[1], pixels.shape[2], pixels.shape[3], 1))
+    elif len(pixels.shape) == 3:
+        pixels = np.reshape(pixels, (pixels.shape[0], pixels.shape[1], pixels.shape[2], 1))
+    return pixels
+
+
+def get_samples(file_path: str, start_window: int, end_window: int):
+    samples = []
+    with open(file_path) as file:
+        windows = file.readlines()[start_window:end_window]
+        for window in windows:
+            sample = np.array(window.split(','), dtype="float64")
+            sample = handle_pixels(sample)
+            samples.append(sample)
+    return reshape(np.array(samples))
+
+
+class SampleHandler:
+
+    def __init__(self, fm_path: str, width: int, height: int):
+        self.fm_path = fm_path
+        self.width = width
+        self.height = height
+
+    def get_datasets(self, start_window: int, end_window: int):
+        indices = []
+        datasets = []
+        for index, file_name in enumerate(sorted_files(self.fm_path)):
+            file_path = get_file_path(self.fm_path, file_name)
+            user_samples = get_samples(file_path, start_window, end_window)
+            if len(user_samples) > 0:
+                indices.append(index)
+                datasets.append(user_samples)
+        return np.array(datasets), np.array(indices)
+
+    def random_dataset(self):
+        def get_random():
+            total_users = len(sorted_files(self.fm_path))
+            file_name = sorted_files(self.fm_path)[random.randrange(total_users)]
+            file_path = get_file_path(self.fm_path, file_name)
+            single_dataset = get_samples(file_path, 0, 1)
+            return single_dataset
+        dataset = get_random()
+        return dataset
+
