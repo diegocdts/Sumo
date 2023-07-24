d03aef9faeab3f7729164cc094852a129877b3ab
Diego Souza <souza.claudiodiego@gmail.com>
2023-07-24 15:19:22 -0300

    comentários em sample_generation

--- FLPUCI/pre_processing/sample_generation.py
+++ FLPUCI/pre_processing/sample_generation.py
@@ -10,6 +10,11 @@ from components.settings import SimulationSettings
 
 
 def logit(cells_list: list):
+    """
+    performs logit transformation over the displacement matrix
+    :param cells_list: the displacement matrix cells as a list
+    :return: the displacement matrix cells transformed
+    """
     for j in range(len(cells_list)):
         p = float(cells_list[j])
         e = float(0)
@@ -23,33 +28,52 @@ def logit(cells_list: list):
 class DisplacementMatrix:
 
     def __init__(self, settings: SimulationSettings):
+        """
+        Instantiates the functions to create a displacement matrix from the trace records
+        :param settings: a SimulationSettings object
+        """
         self.settings = settings
-        self.header = ['win', 'x', 'y', 'time']
+        self.header = ['interval', 'x', 'y', 'time']
 
     def new_record(self, interval: int):
+        """
+        creates a new record in the displacement matrix file
+        :param interval: interval regarding the new record
+        """
         for file_name in sorted_files(self.settings.trace_path):
             file_path = get_file_path(self.settings.trace_path, file_name)
             trace_df = pd.read_csv(file_path, names=self.header)
 
-            fm_file_path = get_file_path(self.settings.dm_path, file_name)
-            fm_df = self.get_matrix(fm_file_path)
+            dm_file_path = get_file_path(self.settings.dm_path, file_name)
+            dm_df = self.get_matrix(dm_file_path)
 
-            this_interval_records = trace_df[trace_df.win == interval]
+            this_interval_records = trace_df[trace_df.interval == interval]
             copy = this_interval_records.copy().reset_index()
 
-            self.fill_record(copy, fm_df, fm_file_path)
+            self.fill_record(copy, dm_df, dm_file_path)
 
-    def get_matrix(self, output_file_path: str):
-        if not os.path.exists(output_file_path):
+    def get_matrix(self, file_path: str):
+        """
+        accesses the displacement matrix
+        :param file_path: path to the displacement matrix to be created or accessed
+        :return: the displacement matrix
+        """
+        if not os.path.exists(file_path):
             columns = np.arange(0, self.settings.width * self.settings.height, 1)
             matrix = pd.DataFrame(columns=columns)
-            matrix.to_csv(output_file_path, index=False)
+            matrix.to_csv(file_path, index=False)
         else:
-            matrix = pd.read_csv(output_file_path)
+            matrix = pd.read_csv(file_path)
         return matrix
 
-    def fill_record(self, this_interval_records: pd.DataFrame, fm_df: pd.DataFrame, output_file_path: str):
-        cell_stay_time = np.zeros(len(fm_df.columns))
+    def fill_record(self, this_interval_records: pd.DataFrame, dm_df: pd.DataFrame, file_path: str):
+        """
+        fills a new displacement matrix record 
+        :param this_interval_records: the trace data regarding the considered interval
+        :param dm_df: the displacement matrix as a DataFrame
+        :param file_path: the displacement matrix path
+        """
+        cell_stay_time = np.zeros(len(dm_df.columns))
 
         if len(this_interval_records) > 0:
             previous_time = this_interval_records.time[0] - 1
@@ -66,18 +90,28 @@ class DisplacementMatrix:
                 cell_stay_time[cell] = new_time
                 previous_time = row.time
 
-            # appends feature_row at the matrix
+        # appends feature_row at the matrix
         cell_stay_time = self.normalize_by_temporal_resolution(cell_stay_time)
-        output_file = open(output_file_path, 'a')
+        output_file = open(file_path, 'a')
         output_file.write(cell_stay_time)
 
     def normalize_by_temporal_resolution(self, cell_stay_time: np.array):
+        """
+        normalizes the new displacement matrix record by the temporal resolution (interval size)
+        :param cell_stay_time: the array representing the cells and the time the user spend in each one of them
+        :return: the new displacement matrix record normalized
+        """
         cells_stay_time_logit = logit([item / self.settings.temporal_resolution for item in cell_stay_time])
         cells_stay_time_str = ', '.join([str(item) for item in cells_stay_time_logit]) + '\n'
         return cells_stay_time_str
 
 
 def handle_pixels(sample_pixels: np.array):
+    """
+    handles the pixels of a sample
+    :param sample_pixels: the sample pixels
+    :return: the sample pixels treated
+    """
     if sample_pixels.max() != sample_pixels.min():
         delta = sample_pixels.max() - sample_pixels.min()
         pixels = np.absolute(sample_pixels.astype("float64")) / delta
@@ -88,20 +122,35 @@ def handle_pixels(sample_pixels: np.array):
     return pixels
 
 
-def reshape(pixels: np.array):
-    if len(pixels.shape) == 4:
-        pixels = np.reshape(pixels, (pixels.shape[0], pixels.shape[1], pixels.shape[2], pixels.shape[3], 1))
-    elif len(pixels.shape) == 3:
-        pixels = np.reshape(pixels, (pixels.shape[0], pixels.shape[1], pixels.shape[2], 1))
-    return pixels
+def reshape(samples: np.array):
+    """
+    reshapes the array of samples by adding the gray scale axis
+    :param samples: the array of samples
+    :return: the array of samples with the gray scale axis
+    """
+    if len(samples.shape) == 4:
+        samples = np.reshape(samples, (samples.shape[0], samples.shape[1], samples.shape[2], samples.shape[3], 1))
+    elif len(samples.shape) == 3:
+        samples = np.reshape(samples, (samples.shape[0], samples.shape[1], samples.shape[2], 1))
+    return samples
 
 
 class SampleHandler:
 
     def __init__(self, settings: SimulationSettings):
+        """
+        Handles the mobility samples
+        :param settings: a SimulationSetting object
+        """
         self.settings = settings
 
     def get_datasets(self, start_window: int, end_window: int):
+        """
+        gets the user datasets. The user datasets may have different number of samples from each other
+        :param start_window: the first interval of the window considered
+        :param end_window: the last interval of the window considered
+        :return: the array of user datasets and a list of indices of users who have not empty datasets
+        """
         indices = []
         datasets = []
         for index, file_name in enumerate(sorted_files(self.settings.dm_path)):
@@ -113,6 +162,13 @@ class SampleHandler:
         return np.array(datasets), np.array(indices)
 
     def get_samples(self, file_path: str, start_window: int, end_window: int):
+        """
+        gets the user samples
+        :param file_path: the displacement matrix file path
+        :param start_window: the first interval of the window considered
+        :param end_window: the last interval of the window considered
+        :return: the array of user samples
+        """
         samples = []
         with open(file_path) as file:
             intervals = file.readlines()[start_window:end_window]
@@ -126,6 +182,10 @@ class SampleHandler:
         return reshape(samples)
 
     def random_dataset(self):
+        """
+        gets a random user dataset
+        :return: the dataset of a random user
+        """
         def get_random():
             total_users = len(sorted_files(self.settings.dm_path))
             file_name = sorted_files(self.settings.dm_path)[random.randrange(total_users)]
@@ -136,6 +196,12 @@ class SampleHandler:
         return dataset
 
     def samples_as_list(self, start_window: int, end_window: int):
+        """
+        gets the user datasets as a list
+        :param start_window: the first interval of the window considered
+        :param end_window: the last interval of the window considered
+        :return: user datasets as a list
+        """
         datasets, indices = self.get_datasets(start_window, end_window)
         samples = []
         for dataset in datasets:
