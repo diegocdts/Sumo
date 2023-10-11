e0c9ba6f5557a1270962520b2c66e8e0a7158d17
Diego Souza <souza.claudiodiego@gmail.com>
2023-10-11 16:59:01 -0300

    TODO done

--- FLPUCI/pre_processing/sample_generation.py
+++ FLPUCI/pre_processing/sample_generation.py
@@ -169,7 +169,7 @@ class SampleHandler:
             user_samples = self.get_samples(file_path, start_window, end_window)
             if len(user_samples) > 0:
                 dataset_id = file_name.replace('.csv', '')
-                indices.append(dataset_id) #TODO: usar o file_name na lista de indices
+                indices.append(dataset_id)
                 datasets.append(user_samples)
             del user_samples
         return datasets, indices
