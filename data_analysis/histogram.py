8008601a0f1a6ac1be8bd32f0e5c5002a3f4f3bf
Diego Souza <souza.claudiodiego@gmail.com>
2024-02-09 13:45:39 -0300

    histogram

--- data_analysis/histogram.py
+++ data_analysis/histogram.py
@@ -1,42 +1,50 @@
+import os
+
 import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib as mpl
 from matplotlib.colors import LightSource
 
+dm_dir = '../_times/sim_24-02-08-01-15-51_dm/'
+dms = sorted(os.listdir(dm_dir), key=lambda x: int(x.replace('.csv', '')[3:]))
 
-dm = np.loadtxt('../_times/sim_24-02-08-01-15-51_dm/veh125.csv', delimiter=',')
-min_stay_time = dm.min()
+for file in dms:
+    dm_path = os.path.join(dm_dir, file)
 
-num_cells = dm.shape[1]
+    dm = np.loadtxt(dm_path, delimiter=',')
+    min_stay_time = dm.min()
 
-x = []
-y = []
+    num_cells = dm.shape[1]
 
-for heatmap in dm:
-    for cell_index, stay_time in enumerate(heatmap):
-        if stay_time > min_stay_time:
-            x_index = cell_index % 128
-            y_index = int(cell_index / 128)
-            x.append(x_index)
-            y.append(y_index)
-x = np.array(x)
-y = np.array(y)
+    x = []
+    y = []
 
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
+    for heatmap in dm:
+        for cell_index, stay_time in enumerate(heatmap):
+            if stay_time > min_stay_time:
+                x_index = cell_index % 128
+                y_index = int(cell_index / 128)
+                x.append(x_index)
+                y.append(y_index)
+    x = np.array(x)
+    y = np.array(y)
 
-hist, xedges, yedges = np.histogram2d(x, y, bins=30)
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
 
-# Construct arrays for the anchor positions of the 16 bars.
-xpos, ypos = np.meshgrid(xedges[:-1] + 0.25, yedges[:-1] + 0.25, indexing="ij")
-xpos = xpos.ravel()
-ypos = ypos.ravel()
-zpos = 0
+    hist, xedges, yedges = np.histogram2d(x, y, bins=30)
 
-# Construct arrays with the dimensions for the 16 bars.
-dx = dy = (xedges.max() - xedges.min()) * 0.02 * np.ones_like(zpos)
-dz = hist.ravel()
+    # Construct arrays for the anchor positions of the 16 bars.
+    xpos, ypos = np.meshgrid(xedges[:-1] + 0.25, yedges[:-1] + 0.25, indexing="ij")
+    xpos = xpos.ravel()
+    ypos = ypos.ravel()
+    zpos = 0
 
-ax.bar3d(xpos, ypos, zpos, dx, dy, dz, zsort='average')
+    # Construct arrays with the dimensions for the 16 bars.
+    dx = dy = (xedges.max() - xedges.min()) * 0.02 * np.ones_like(zpos)
+    dz = hist.ravel()
 
-plt.show()
+    ax.bar3d(xpos, ypos, zpos, dx, dy, dz, zsort='average')
+    fig.suptitle(file.replace('.csv', ''))
+
+    plt.show()
